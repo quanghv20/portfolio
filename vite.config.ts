4ff7695a7d02a43892d795d8299d7a6b2626a36f
quanghv20 <quanghv20@gmail.com>
2025-05-11 15:15:17 +0700

    config alias path

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      // "@constants": path.resolve(__dirname, "src/constants"),
-      // "@components": path.resolve(__dirname, "src/components"),
-      // "@pages": path.resolve(__dirname, "src/pages"),
-      // "@utils": path.resolve(__dirname, "src/utils"),
     },
   },
 });
